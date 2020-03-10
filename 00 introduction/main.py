f5085e01c5e9fb89fa1cb0b63876e1beaf3ac782
zronghui <825503975@qq.com>
2020-03-10 10:13:07 +0800

    .

--- 00 introduction/main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import re
-
-regex = r'1995年6月1日|1995/6/1|1995\-6\-1|1995\-06\-01|1995\-06'
-s = '•”xxx出生于1995年6月1日” • ”xxx出生于1995/6/1” • ”xxx出生于1995-6-1” • ”xxx出生于1995-06-01” • ”xxx出生于1995-06”'
-print(re.findall(regex, s))
