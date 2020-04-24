8c43323a4b6a3c1d34b252c9fdb68847f7753eaf
zronghui <825503975@qq.com>
2020-04-24 19:23:15 +0800

    添加 crontab 命令
    Took 1 minute

--- crontab.sh
+++ crontab.sh
@@ -5,4 +5,7 @@ scrapy crawl books
 scrapy crawl movies
 cd ../other || exit
 python3 es\ store\ data.py
+
+# crontab -e
+# xxxt=/root/code/zronghui_xxxt
 # 0 */1 * * * cd $xxxt && ./crontab.sh >> $xxxt/crontab.log 2>&1
