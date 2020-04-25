f449e7cde33ad5d9ea35e50fb2433c36038f664f
zronghui <825503975@qq.com>
2020-04-25 23:44:24 +0800

    elasticdump limit 1000
    Took 10 minutes

--- esExportJson.sh
+++ esExportJson.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-elasticdump \
+elasticdump --limit 1000 \
   --input=http://127.0.0.1:9200/movies \
   --output=./movies_mapping.json \
   --type=mapping
-elasticdump \
+elasticdump --limit 1000 \
   --input=http://127.0.0.1:9200/movies \
   --output=./movies.json \
   --type=data
 
-elasticdump \
+elasticdump --limit 1000 \
   --input=http://127.0.0.1:9200/books \
   --output=./books_mapping.json \
   --type=mapping
-elasticdump \
+elasticdump --limit 1000 \
   --input=http://127.0.0.1:9200/books \
   --output=./books.json \
   --type=data
