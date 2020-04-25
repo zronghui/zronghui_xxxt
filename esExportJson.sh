40f081753b9b943befaa92234a0af5d2eae037ad
zronghui <825503975@qq.com>
2020-04-25 23:52:51 +0800

    elasticdump concurrentcy and overwrite
    Took 6 minutes

--- esExportJson.sh
+++ esExportJson.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-elasticdump --limit 1000 \
+elasticdump --limit 1000 --concurrency 4 --overwrite \
   --input=http://127.0.0.1:9200/movies \
   --output=./movies_mapping.json \
   --type=mapping
-elasticdump --limit 1000 \
+elasticdump --limit 1000 --concurrency 4 --overwrite \
   --input=http://127.0.0.1:9200/movies \
   --output=./movies.json \
   --type=data
 
-elasticdump --limit 1000 \
+elasticdump --limit 1000 --concurrency 4 --overwrite \
   --input=http://127.0.0.1:9200/books \
   --output=./books_mapping.json \
   --type=mapping
-elasticdump --limit 1000 \
+elasticdump --limit 1000 --concurrency 4 --overwrite \
   --input=http://127.0.0.1:9200/books \
   --output=./books.json \
   --type=data
