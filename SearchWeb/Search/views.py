b5045372410ca3f7607bf0f3e6ce3a578fa7bb8d
zronghui <825503975@qq.com>
2020-04-26 14:08:39 +0800

    长度限制 20->40
    Took 19 seconds

--- SearchWeb/Search/views.py
+++ SearchWeb/Search/views.py
@@ -17,8 +17,8 @@ def search(request):
     pageNo = int(request.GET.get('pageno', 0))
     q = request.GET.get('q')
     search_type = request.GET.get('search_type', 'movies')
-    # 限定搜索词长度在 1~20 之间
-    if not 1 < len(q) < 20 or search_type not in ['movies', 'books']:
+    # 限定搜索词长度在 1~40 之间
+    if not 1 < len(q) < 40 or search_type not in ['movies', 'books']:
         search_result = {
             'hits': {'total': 0, 'hits': []},
             'took': 0
