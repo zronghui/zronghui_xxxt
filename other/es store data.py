5cde99b08ea62990fb58714ba731de42e19230a0
zronghui <825503975@qq.com>
2020-04-28 22:12:19 +0800

    fix
    Took 5 minutes

--- other/es store data.py
+++ other/es store data.py
@@ -70,7 +70,7 @@ def bulk_with_json(jsonFile, doc_type):
                 num += 1
                 logger.debug("Insert " + str(num * max_count) + " records.")
     bulk(actions)
-    logger.info(f'success insert {num} items')
+    logger.info(f'success insert {num * max_count + count} items')
 
 
 def bulk(actions):
