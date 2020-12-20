3fc39e35a5a079838c2658593402e0d8a06055d1
zronghui <825503975@qq.com>
2020-12-20 18:54:19 +0800

    set redis password

--- SearchWeb/utils/redis_utils.py
+++ SearchWeb/utils/redis_utils.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+
 import pretty_errors
 import redis
+from environs import Env
 
 pretty_errors.activate()
 
+env = Env()
+env.read_env()
+
 host = '127.0.0.1'
 port = 6379
-r = redis.StrictRedis(host=host, port=port)
+if not env.str('redispassword', ''):
+    print('请在终端设置 Redis 密码')
+    sys.exit(-1)
+r = redis.StrictRedis(host=host, port=port, password=env.str('redispassword', ''))
 
 
 # def get_hot_search_words():
