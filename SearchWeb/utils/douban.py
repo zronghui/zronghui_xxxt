09feb2be21dda300cc83377a38a9076bb1b32f22
zronghui <825503975@qq.com>
2020-12-20 20:01:31 +0800

    remove douban token api

--- SearchWeb/utils/douban.py
+++ SearchWeb/utils/douban.py
@@ -9,7 +9,7 @@ import json
 import itertools
 import functools
 
-import config
+# import config
 
 pretty_errors.activate()
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) '
@@ -17,35 +17,36 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) '
                          '1.0.4044.122 Safari/537.36'}
 
 
-@functools.lru_cache
-def getMovieId(q, apikey):
-    try:
-        r = requests.get('https://api.douban.com/v2/movie/search?q={q}&apikey={apikey}'.
-                         format(q=q, apikey=apikey),
-                         headers=headers)
-        return r.json()['subjects'][0]['id']
-    except Exception as e:
-        # api 请求失败，直接发送请求
-        logger.error(e)
-        r = requests.get('https://www.douban.com/search?q='+q, headers=headers)
-        return re.findall(r'com%2Fsubject%2F(.*?)%2F', r.text)[0]
+@functools.lru_cache(maxsize=128)
+def getMovieId(q):
+    # try:
+    #     r = requests.get('https://api.douban.com/v2/movie/search?q={q}&apikey={apikey}'.
+    #                      format(q=q, apikey=apikey),
+    #                      headers=headers)
+    #     return r.json()['subjects'][0]['id']
+    # except Exception as e:
+    # # api 请求失败，直接发送请求
+    # logger.error(e)
+    r = requests.get('https://www.douban.com/search?q=' + q, headers=headers)
+    return re.findall(r'com%2Fsubject%2F(.*?)%2F', r.text)[0]
 
 
-@functools.lru_cache
-def getMovieDetail(movie_id, apikey):
-    r = requests.get('https://api.douban.com/v2/movie/subject/{movie_id}?apikey={apikey}'.
-                     format(movie_id=movie_id, apikey=apikey),
-                     headers=headers)
-    if 'id' in r.json():
-        return r.json()
-    # api 请求失败，直接发送请求
+@functools.lru_cache(maxsize=128)
+def getMovieDetail(movie_id):
+    # r = requests.get('https://api.douban.com/v2/movie/subject/{movie_id}?apikey={apikey}'.
+    #                  format(movie_id=movie_id, apikey=apikey),
+    #                  headers=headers)
+    # if 'id' in r.json():
+    #     return r.json()
+    # # api 请求失败，直接发送请求
     r = requests.get('https://movie.douban.com/subject/' +
                      str(movie_id), headers=headers)
     r = json.loads(re.findall(r'<script type="application/ld\+json">(.*?)</script>',
                               r.text.replace('\n', ''))[0])
     # print(json.dumps(r, ensure_ascii=False, indent=2))
     for i in itertools.chain(r['director'], r['actor']):
-        i['name'] = i['name'].split()[0]
+        i['name'] = ' '.join(i['name'].split()[:1])
+        print(i['name'])
     res = {
         'title': r['name'],
         'id': str(movie_id),
@@ -60,12 +61,12 @@ def getMovieDetail(movie_id, apikey):
     return res
 
 
-def getMovieDetailByQuery(q):
-    try:
-        movie_id = getMovieId(q, config.doubanApikey)
-        return getMovieDetail(movie_id, config.doubanApikey)
-    except Exception as e:
-        logger.error(e)
+# def getMovieDetailByQuery(q):
+#     try:
+#         movie_id = getMovieId(q, config.doubanApikey)
+#         return getMovieDetail(movie_id, config.doubanApikey)
+#     except Exception as e:
+#         logger.error(e)
 
 
 # def main():
@@ -75,4 +76,4 @@ def getMovieDetailByQuery(q):
 #     logger.debug(getMovieDetail(movie_id, config.doubanApikey))
 
 if __name__ == '__main__':
-    print(getMovieDetail(getMovieId('摩天大楼', ''), ''))
+    print(getMovieDetail(getMovieId('摩天大楼')))
