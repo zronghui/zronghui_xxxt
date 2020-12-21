94a3ceca6b52d4d6c1f8273cbb6ea98e8347ba58
zronghui <825503975@qq.com>
2020-12-21 11:26:54 +0800

    update crontab shell

--- crontab.sh
+++ crontab.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
+InCrontab=true
+
 #python other/checkService.py
 cd helloScrapy || exit
-scrapy crawl movies./
+
+while true
+do
+    echo 'scrapy begin...'
+    scrapy crawl movies
+    for i in seq(1 300)
+    do
+        echo 'sleep '$i'/300s'
+        sleep 1
+    done
+done
 #cd ../other || exit
 #python3 es\ store\ data.py
 
